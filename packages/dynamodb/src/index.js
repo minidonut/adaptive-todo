9b8d55c72da45b43faf792f34326b898c3cca80d
minidonut <nycom13@gmail.com>
2020-08-25 03:23:42 +0900

    update dependencies

--- packages/dynamodb/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = void 0;
-const test = () => {
-    console.log('dynamodb packages');
-};
-exports.test = test;
